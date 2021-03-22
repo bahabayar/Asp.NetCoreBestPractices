5982d2a3fef9ac25daabb217205d28d95deacc45
Baha <bahabayar@hotmail.com>
2021-03-22 14:22:43 +0300

    Product ve Category Moedllerimizin oluşturulması

---
+++ Asp.NetCoreBestPractices.Core/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Asp.NetCoreBestPractices.Core.Models
+{
+   public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted  { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
