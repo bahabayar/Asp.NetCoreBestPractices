26889bd7eb656b231c1fcc91aab652bb38479bc9
Baha <bahabayar@hotmail.com>
2021-03-23 16:30:33 +0300

    IUnitOfWork interface'sinin oluşturulması

---
+++ Asp.NetCoreBestPractices.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Asp.NetCoreBestPractices.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreBestPractices.Core.UnitOfWork
+{
+   public interface IUnitOfWork
+    {
+        IProductRepository Product { get; }
+        ICategoryRepository Category { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
