290f59c4d1aacaed63ea09187bfd1ef4a2480de3
Baha <bahabayar@hotmail.com>
2021-03-25 23:12:53 +0300

    Startup.cs 'de değişiklikler

--- Asp.NetCoreBestPractices.API/Startup.cs
+++ Asp.NetCoreBestPractices.API/Startup.cs
@@ -1,7 +1,11 @@
+using Asp.NetCoreBestPractices.Core.UnitOfWork;
+using Asp.NetCoreBestPractices.Data;
+using Asp.NetCoreBestPractices.Data.UnitOfWorks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +29,12 @@ namespace Asp.NetCoreBestPractices.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => 
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:SqlConstr"].ToString());
+            
+            });
+            services.AddScoped<IUnitOfWork, UnitOfWork>(); 
             services.AddControllers();
         }
 
