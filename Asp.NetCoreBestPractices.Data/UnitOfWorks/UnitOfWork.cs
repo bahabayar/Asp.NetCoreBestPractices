d2d3ac498a6a86f9b13b8a66163ed4774366855f
Baha <bahabayar@hotmail.com>
2021-03-25 22:05:36 +0300

    Data katmanı UnitOfWork

---
+++ Asp.NetCoreBestPractices.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Asp.NetCoreBestPractices.Core.Repositories;
+using Asp.NetCoreBestPractices.Core.UnitOfWork;
+using Asp.NetCoreBestPractices.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreBestPractices.Data.UnitOfWorks
+{
+   public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+
+        public IProductRepository Products => _productRepository = _productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository Categories => _categoryRepository = _categoryRepository ?? new CategoryRepository(_context);
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()//saveChanges
+        {
+           _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
