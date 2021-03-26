80b0ab463df1bcfa53ec59a319e7907f0b335e72
Baha <bahabayar@hotmail.com>
2021-03-26 16:35:34 +0300

    Service class'ının yazılması

--- Asp.NetCoreBestPractices.Service/Services/ProductService.cs
+++ Asp.NetCoreBestPractices.Service/Services/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Asp.NetCoreBestPractices.Core.Models;
+using Asp.NetCoreBestPractices.Core.Repositories;
 using Asp.NetCoreBestPractices.Core.Services;
 using Asp.NetCoreBestPractices.Core.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
@@ -10,71 +11,15 @@ using System.Threading.Tasks;
 
 namespace Asp.NetCoreBestPractices.Service.Services
 {
-    class ProductService : IProductService
+    public class ProductService : Service<Product>, IProductService
     {
-        private readonly IUnitOfWork _unitOfWork;
-        public ProductService(IUnitOfWork unitOfWork)
+        public ProductService(IUnitOfWork unitOfWork, IRepository<Product> repository) : base(unitOfWork, repository)
         {
-            _unitOfWork = unitOfWork;
-        }
-
-        public async Task<Product> AddAsync(Product entity)
-        {
-            await _unitOfWork.Products.AddAsync(entity);
-            await _unitOfWork.CommitAsync();
-            return entity;
-        }
-
-        public async Task<IEnumerable<Product>> AddRangeASync(IEnumerable<Product> entities)
-        {
-            await _unitOfWork.Products.AddRangeASync(entities);
-            await _unitOfWork.CommitAsync();
-            return entities;
-
-        }
-
-        public async Task<IEnumerable<Product>> Where(Expression<Func<Product, bool>> predicate)
-        {
-            return await _unitOfWork.Products.Where(predicate);
-        }
-
-        public async Task<IEnumerable<Product>> GetAllAsync()
-        {
-            return await _unitOfWork.Products.GetAllAsync();
-        }
-
-        public async Task<Product> GetByIdAsync(int id)
-        {
-            return await _unitOfWork.Products.GetByIdAsync(id);
         }
 
         public async Task<Product> GetWithCategoryByIdAsync(int productId)
         {
-            return await _unitOfWork.Products.GetWithCategoryByIdAsync(productId);
-        }
-
-        public void Remove(Product entity)
-        {
-            _unitOfWork.Products.Remove(entity);
-            _unitOfWork.Commit();
-        }
-
-        public void RemoveRange(IEnumerable<Product> entities)
-        {
-            _unitOfWork.Products.RemoveRange(entities);
-            _unitOfWork.Commit();
-        }
-
-        public async Task<Product> SingleOrDefaultAsync(Expression<Func<Product, bool>> predicate)
-        {
-            return await _unitOfWork.Products.SingleOrDefaultAsync(predicate);
-        }
-
-        public Product Update(Product entity)
-        {
-            var updateProduct = _unitOfWork.Products.Update(entity);
-            _unitOfWork.Commit();
-            return updateProduct;
+           return await _unitOfWork.Products.GetWithCategoryByIdAsync(productId);
         }
     }
 }
---
+++ Asp.NetCoreBestPractices.Service/Services/Service.cs
@@ -0,0 +1,76 @@
+﻿using Asp.NetCoreBestPractices.Core.Repositories;
+using Asp.NetCoreBestPractices.Core.Services;
+using Asp.NetCoreBestPractices.Core.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreBestPractices.Service.Services
+{
+    public class Service<TEntity> : IService<TEntity> where TEntity : class
+    {
+        public readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<TEntity> _repository;
+        public Service(IUnitOfWork unitOfWork,IRepository<TEntity> repository)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+        }
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> AddRangeASync(IEnumerable<TEntity> entities)
+        {
+            await _repository.AddRangeASync(entities);
+            await _unitOfWork.CommitAsync();
+            return entities;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+           return await _repository.GetAllAsync();
+            
+            
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _repository.Remove(entity);
+            _unitOfWork.Commit();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _repository.RemoveRange(entities);
+            _unitOfWork.Commit();
+        }
+
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+           return await _repository.SingleOrDefaultAsync(predicate);
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            TEntity updateEntity = _repository.Update(entity);
+            _unitOfWork.Commit();
+            return updateEntity;
+        }
+
+        public async Task<IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _repository.Where(predicate);
+        }
+    }
+}
