29ec89117a4a657503883132cacb35221a7d9dcf
Baha <bahabayar@hotmail.com>
2021-03-27 16:07:55 +0300

    ProductsController'da GetById  action methodunun oluşturulması

--- Asp.NetCoreBestPractices.API/Controllers/CategoriesController.cs
+++ Asp.NetCoreBestPractices.API/Controllers/CategoriesController.cs
@@ -13,11 +13,11 @@ namespace Asp.NetCoreBestPractices.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoryController : ControllerBase
+    public class CategoriesController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
         public readonly IMapper _mapper;
-        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
             _mapper = mapper;
---
+++ Asp.NetCoreBestPractices.API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Asp.NetCoreBestPractices.API.DTOs;
+using Asp.NetCoreBestPractices.Core.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreBestPractices.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly  IProductService _productService;
+        public readonly IMapper _mapper;
+        public ProductsController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            return Ok(_mapper.Map<ProductDto>(product));
+        }
+    }
+}
