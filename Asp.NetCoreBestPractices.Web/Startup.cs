78f422b0f409e72a3c19c4b411812f0d4f0c5ab6
Baha <bahabayar@hotmail.com>
2021-04-05 14:59:29 +0300

    Startup.cs dosyasının güncellenmesi

--- Asp.NetCoreBestPractices.Web/Startup.cs
+++ Asp.NetCoreBestPractices.Web/Startup.cs
@@ -1,6 +1,14 @@
+using Asp.NetCoreBestPractices.Core.Repositories;
+using Asp.NetCoreBestPractices.Core.Services;
+using Asp.NetCoreBestPractices.Core.UnitOfWork;
+using Asp.NetCoreBestPractices.Data;
+using Asp.NetCoreBestPractices.Data.Repository;
+using Asp.NetCoreBestPractices.Data.UnitOfWorks;
+using Asp.NetCoreBestPractices.Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +31,21 @@ namespace Asp.NetCoreBestPractices.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(IService<>), typeof(Service.Services.Service<>));
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:SqlConstr"].ToString(), o =>
+                {
+
+                    o.MigrationsAssembly("Asp.NetCoreBestPractices.Data");
+                });
+
+            });
             services.AddControllersWithViews();
         }
 
