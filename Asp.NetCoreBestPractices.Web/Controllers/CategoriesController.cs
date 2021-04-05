6089338fd42880328c390df22e67f3621b26058a
Baha <bahabayar@hotmail.com>
2021-04-05 18:16:53 +0300

    CategoriesController sınıfının oluşturulması

---
+++ Asp.NetCoreBestPractices.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Asp.NetCoreBestPractices.Core.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreBestPractices.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService,IMapper mapper)
+        {
+            _mapper = mapper;
+            _categoryService = categoryService;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
